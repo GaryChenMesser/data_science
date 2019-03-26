d8ec4bdf04ef9ddd6a96cfc422a81f670c828b0d
GaryChenMesser <gary9192631770@gmail.com>
2019-03-26 21:29:47 +0800

    debug for pr8

--- hw5_pr8/lasso.py
+++ hw5_pr8/lasso.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-n = 500000
+n = 500
 beta_true = np.array([-2, -2, 2, 2, -2])
-X = np.zeros((n, 500), dtype=np.float16)
+X = np.zeros((n, 500), dtype=np.float32)
 X = np.random.normal(0, 1, (n, 500))
 y = np.matmul(X[:, ::100], beta_true) + np.random.normal(0, 0.05, (n))
 
 Lambda = 0
 rho = 1
-m = 100
+m = 10
 row = n // m
 
 theta = [np.zeros((m, 500)) for j in range(2)]
@@ -17,12 +17,13 @@ alpha = [np.zeros((m, 500)) for j in range(2)]
 
 
 inverse = []
-XTy = np.matmul(X.T, y)
+XTy = []
 
 print("test")
 
 for i in range(m):
     inverse.append(np.linalg.inv(np.matmul(X[i * row:(i + 1) * row, :].T, X[i * row:(i + 1) * row, :]) + rho * np.identity(500)))
+    XTy.append(np.matmul(X[i * row:(i + 1) * row, :].T, y[i * row:(i + 1) * row]))
 
 print("test2")
 
@@ -41,7 +42,7 @@ for Lambda in [1]:
         print(np.mean(theta[0][:, ::100], axis=0))
         
         for i in range(m):
-            theta[1][i] = np.matmul(inverse[i], (XTy + rho * (beta[0] - alpha[0][i])))
+            theta[1][i] = np.matmul(inverse[i], (XTy[i] + rho * (beta[0] - alpha[0][i])))
     
         _theta = np.mean(theta[1], axis=0)
         _alpha = np.mean(alpha[0], axis=0)
@@ -50,7 +51,7 @@ for Lambda in [1]:
         for i in range(m):
             alpha[1][i] = alpha[0][i] + theta[1][i] - beta[1]
             
-        t = np.linalg.norm(np.mean([theta[1][i] - beta for i in range(m)], axis=0))
+        t = np.linalg.norm(np.mean([theta[1][i] - beta[1] for i in range(m)], axis=0))
         s = np.linalg.norm(beta[0] - beta[1])
         print(t)
         print(s)
@@ -59,9 +60,12 @@ for Lambda in [1]:
         
         if t > 10 * s:
             rho *= 2
+            for i in range(m):
+                alpha[1][i] /= 2
         elif s > 10 * t:
             rho /= 2
-
+            for i in range(m):
+                alpha[1][i] *= 2
 
 
 
