259ea69d483dfc11a215d4e29952b42dc0058461
GaryChenMesser <gary9192631770@gmail.com>
2019-04-02 23:43:07 +0800

    update

--- hw5_pr8/lasso.py
+++ hw5_pr8/lasso.py
@@ -5,7 +5,7 @@ parser = ArgumentParser()
 parser.add_argument("--mode", type=int)
 args = parser.parse_args()
 
-n = 300000
+n = 10000
 beta_true = np.array([-2, -2, 2, 2, -2])
 X = np.zeros((n, 500), dtype=np.float32)
 X = np.random.normal(0, 1, (n, 500))
@@ -13,7 +13,7 @@ y = np.matmul(X[:, ::100], beta_true) + np.random.normal(0, 0.05, (n))
 
 Lambda = 0
 rho = 1
-m = 3000
+m = 100
 row = n // m
 
 inverse = []
@@ -46,8 +46,8 @@ for index, Lambda in enumerate(lam):
     s_stop = 0
     
     ite = 0
-    while ite < 500:
-    #while t > t_stop or s > s_stop:
+    #while itr < 500:
+    while(t > t_stop or s > s_stop):
         ite += 1
         if ite % 100 == 0:
             print(ite)
@@ -60,27 +60,26 @@ for index, Lambda in enumerate(lam):
         
         for i in range(m):
             theta[1][i] = np.matmul(inverse[i], (XTy[i] + rho * (beta[0] - alpha[0][i])))
-    
+        
         _theta = np.mean(theta[1], axis=0)
         _alpha = np.mean(alpha[0], axis=0)
         beta[1] = np.sign(_theta + _alpha) * np.maximum(np.zeros(_theta.shape), np.abs(_theta + _alpha) - Lambda / rho / m)
- 
+    
         for i in range(m):
             alpha[1][i] = alpha[0][i] + theta[1][i] - beta[1]
             
         t = np.linalg.norm(np.mean([theta[1][i] - beta[1] for i in range(m)], axis=0))
         s = np.linalg.norm(beta[0] - beta[1]) * rho
-        
         #print(t)
-        #print(s)        
-
+        #print(s)
+        
         if args.mode == 1:
             t_list[index].append(t)
             s_list[index].append(s)
         
-        t_stop = 0.0001 * np.linalg.norm(beta[1])
-        s_stop = 0.0001 * np.linalg.norm(np.mean(alpha[1], axis=0))
-        
+        t_stop = 0.001 * np.linalg.norm(beta[1])
+        s_stop = 0.001 * np.linalg.norm(np.mean(alpha[1], axis=0))
+    
         if t > 10 * s:
             rho *= 2
             for i in range(m):
