40b4d7f256649d8a01652337a06b01dd6c068895
GaryChenMesser <gary9192631770@gmail.com>
2019-04-02 09:56:45 +0000

    update for plot and plot2's output

--- hw5_pr8/lasso.py
+++ hw5_pr8/lasso.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-n = 3000
+n = 300
 beta_true = np.array([-2, -2, 2, 2, -2])
 X = np.zeros((n, 500), dtype=np.float32)
 X = np.random.normal(0, 1, (n, 500))
@@ -8,7 +8,7 @@ y = np.matmul(X[:, ::100], beta_true) + np.random.normal(0, 0.05, (n))
 
 Lambda = 0
 rho = 1
-m = 1000
+m = 100
 row = n // m
 
 theta = [np.zeros((m, 500)) for j in range(2)]
@@ -27,10 +27,13 @@ for i in range(m):
 
 print("ADMM start!")
 
-lam = [0.5, 1, 2, 4]
-t_list = [[] for i in lam]
-s_list = [[] for i in lam]
+lam = np.linspace(0.001, 5, 20)
+beta_list = []
+#lam = [0.5, 1, 2, 4]
+#t_list = [[] for i in lam]
+#s_list = [[] for i in lam]
 for index, Lambda in enumerate(lam):
+    print("lambda = ", Lambda)
     t = 1
     s = 1
     t_stop = 0
@@ -53,14 +56,14 @@ for index, Lambda in enumerate(lam):
         _theta = np.mean(theta[1], axis=0)
         _alpha = np.mean(alpha[0], axis=0)
         beta[1] = np.sign(_theta + _alpha) * np.maximum(np.zeros(_theta.shape), np.abs(_theta + _alpha) - Lambda / rho / m)
-    
+ 
         for i in range(m):
             alpha[1][i] = alpha[0][i] + theta[1][i] - beta[1]
             
         t = np.linalg.norm(np.mean([theta[1][i] - beta[1] for i in range(m)], axis=0))
         s = np.linalg.norm(beta[0] - beta[1])
-        t_list[index].append(t)
-        s_list[index].append(s)
+        #t_list[index].append(t)
+        #s_list[index].append(s)
         t_stop = 0.0001 * np.linalg.norm(beta[1])
         s_stop = 0.0001 * np.linalg.norm(np.mean(alpha[1], axis=0))
         
@@ -72,11 +75,11 @@ for index, Lambda in enumerate(lam):
             rho /= 2
             for i in range(m):
                 alpha[1][i] *= 2
+    
+    beta_list.append(beta[1])
 
-with open("out", 'w') as f:
+with open("out2", 'w') as f:
     f.write(repr(lam))
     f.write('\n')
-    f.write(repr(t_list))
+    f.write(repr(beta_list))
     f.write('\n')
-    f.write(repr(s_list))
-
